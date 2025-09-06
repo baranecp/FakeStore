f8f2ace93c256bb6690297ced6f5bcc447f2c08f
baranecp <baranec.dev@gmail.com>
2025-09-06 14:17:01 +0200

    Create custom hook for fetching data

---
+++ services/useFetchProducts.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+const BASE_URL = "https://fakestoreapi.com";
+
+const FetchData = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchDataStore = async () => {
+      try {
+        const response = await fetch(`${BASE_URL}/products`);
+        if (!response.ok) {
+          throw new Error(`HTTP error: Status ${response.status}`);
+        }
+        let fetchData = await response.json();
+        setData(fetchData);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchDataStore();
+  }, []);
+
+  return { data, loading, error };
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import FetchData from '../services/FetchStore'
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <h1>FakeStore</h1>
+      <FetchData />
     </>
   )
 }
