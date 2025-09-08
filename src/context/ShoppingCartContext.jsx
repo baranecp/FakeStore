06d0e6d6cca0be40c0fa0e3dfbb1f2e54c7184b9
baranecp <baranec.dev@gmail.com>
2025-09-08 15:59:18 +0200

    Add context api

---
+++ src/context/ShoppingCartContext.jsx
@@ -0,0 +1,47 @@
+import { useFetchProducts } from "@/services/useFetchProducts";
+import { useContext, createContext, useState } from "react";
+const ShoppingCartContext = createContext({})
+
+export function ShoppingCartProvider({ children }) {
+    const { data, loading, error } = useFetchProducts();
+    const [cartItems, setCartItems] = useState([])
+
+    function getItemQuantity(id) {
+        return cartItems.find(item => item.id === id)?.quantity || 0;
+    }
+
+    function increaseCartQuantity(id) {
+        setCartItems(currItems => {
+            if (currItems.find(item => item.id === id) == null) {
+                return [...currItems, { id, quantity: 1 }];
+            } else {
+                return currItems.map(item =>
+                    item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+                );
+            }
+        });
+    }
+
+    function decreaseCartQuantity(id) {
+        setCartItems(currItems => {
+            if (currItems.find(item => item.id === id)?.quantity === 1) {
+                return currItems.filter(item => item.id !== id);
+            } else {
+                return currItems.map(item =>
+                    item.id === id ? { ...item, quantity: item.quantity - 1 } : item
+                );
+            }
+        });
+    }
+
+    function removeFromCart(id) {
+        setCartItems(currItems => currItems.filter(item => item.id !== id));
+    }
+
+    return <ShoppingCartContext.Provider value={{ data, loading, error, cartItems, getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart }}>{children}</ShoppingCartContext.Provider>
+
+}
+
+export function useShoppingCart() {
+    return useContext(ShoppingCartContext)
+}
